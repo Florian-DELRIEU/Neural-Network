a18d20c853acba184a6d06282c1ad40b3a5b342a
floriandelrieu <florian.delrieu@free.fr>
2021-06-21 14:58:07 +0200

    Begin to write Simple_Network

--- Simple_Network.py
+++ Simple_Network.py
@@ -1,8 +1,21 @@
 """
 Premier essai de réseaux de neurones
 """
+import numpy as np
+
 class InitialLayer:
     def __init__(self,N):
-        self.input = list()
-        self.output = list()
+        self.input = None
+        self.output = None
 
+    def FeedForward(self,Input):
+        """
+        :Input: liste de taille N (autant d'entrée que de neurones)
+        """
+        self.input = Input
+        self.output = self.input
+
+class Layer:
+    def __init__(self,N):
+        self.input = None
+        self.output = None
