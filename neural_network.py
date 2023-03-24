e4d0eb8eaecbe4218dc19da5540906b2c125cc40
Florian Delrieu <florian.delrieu@free.fr>
2023-03-24 21:31:22 +0100

    [GPT-v1.1.1C] - Decimal Sorter
    - back to original derivate function
    - add FIXMEs
    - fix plot function
    - change parameters
    - add new data

--- neural_network.py
+++ neural_network.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-DEBUG_ON = False
-PLOTTING_ON = False
+DEBUG_ON = True
+PLOTTING_ON = True
 
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size):
@@ -11,7 +11,7 @@ class NeuralNetwork:
         self.bias1 = np.zeros((1, hidden_size))  # biais de la couche d'entrée (constantes)
         self.bias2 = np.zeros((1, output_size))  # biais de la couche cachée
 
-    def function(self, x, fonction_to_use="linear"):
+    def function(self, x, fonction_to_use="sigmoid"):
         """
         Calcule la fonction d'activation sigmoid sur une entrée x.
             - x (numpy.ndarray): Un scalaire, un vecteur ou une matrice d'entrée.
@@ -45,12 +45,12 @@ class NeuralNetwork:
             learning_rate (float): Le taux d'apprentissage du réseau de neurones.
         """
         # rétropropagation du gradient
-        d_output = (y - self.output)/y
-        d_hidden = d_output*self.weights2.T/self.hidden
+        d_output = (y - self.output)*self.output*(1 - self.output)
+        d_hidden = np.dot(d_output, self.weights2.T)*self.hidden*(1 - self.hidden)
 
         # mise à jour des poids et des biais
-        self.weights2 += learning_rate*np.dot(self.hidden.T,d_output)
-        self.weights1 += learning_rate*np.dot(X.T,d_hidden)
+        self.weights2 += learning_rate*np.dot(self.hidden.T,d_output) #fixme
+        self.weights1 += learning_rate*np.dot(X.T,d_hidden) #fixme
         self.bias2 += learning_rate*np.sum(d_output, axis=0)
         self.bias1 += learning_rate*np.sum(d_hidden, axis=0)
 
@@ -63,25 +63,30 @@ class NeuralNetwork:
             y (ndarray): Valeurs cibles correspondant à chaque observation dans `X`.
             learning_rate (float): Taux d'apprentissage utilisé pour mettre à jour les poids du réseau.
             epochs (int): Nombre d'itérations d'entraînement à effectuer.
+        FIXME
+            - Apparition de nan values a partir du premier loop.
+            - Une de ces raison etait la multiplication par 0 qui survennait lors du calcul de l'erreur
+            - Peut être est ce encore les poids ?
+            - J'ai mis des tags
         """
         for i in range(epochs):
             if DEBUG_ON: print(f"Training... epoch {i}")
             self.forward(X)
             self.backward(X, y, learning_rate)
             if PLOTTING_ON:
-                self.plot_output(i,self.output,"Output")
-                self.plot_output(i,self.weights1,"Weigths")
-                self.plot_output(i,self.weights2,"Weigths")
+                self.plot_output(self.output,"Output")
+                self.plot_output(self.weights1,"Weigths")
+                self.plot_output(self.weights2,"Weigths")
 
 
     def predict(self, X):
         self.forward(X)
         return self.output
 
-    def plot_output(self,values,i,fig_name=None):
+    def plot_output(self,values,fig_name=None):
         if fig_name is None:    plt.figure()
         else:                   plt.figure(fig_name)
-        plt.plot(values,i,"kx")
+        plt.plot(values,"kx")
         print(self.output)
 
     def input(self,X):
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from neural_network import NeuralNetwork
 import numpy as np
 
 input_size = 1 # taille de la couche d'entrée
-hidden_size = 2 # taille de la couche cachée
+hidden_size = 5 # taille de la couche cachée
 output_size = 1 # taille de la couche de sortie
 Network = NeuralNetwork(input_size, hidden_size, output_size)
 
@@ -12,13 +12,18 @@ Préparez vos données d'entraînement. Dans cet exemple, nous allons utiliser d
 Vous pouvez utiliser vos propres données en les chargeant depuis un fichier ou en les générant de manière programmée. 
 Nous allons également diviser les données en ensembles d'entraînement et de validation :
 """
-X = np.array([0.2])# données d'entrée
-y = 2*X # valeurs cibles
+X = np.array([ 0.45375244, -0.87447848,  0.20418979,  0.22612347, -0.82194152,
+        0.76487173, -0.01377334, -0.25665163, -0.72822261, -0.44119749,
+        0.87519806, -0.94695929, -0.53262514, -0.7910783 , -0.01285381,
+        0.32680019,  0.04060011, -0.58239623, -0.52266407, -0.83104156,
+       -0.06727012, -0.80390909,  0.24674281,  0.26055366, -0.1371677 ,
+       -0.88861433,  0.19587372,  0.98332952, -0.21530903,  0.64215399]).reshape(30,1)
+y = np.where(X < -0.3, -1, np.where(X > 0.3, 1, 0))
 
 """
 Entraînez le système neuronal en appelant la méthode train de l'instance de NeuralNetwork :
 """
-learning_rate = 0.1 # taux d'apprentissage
-epochs = 200 # nombre d'itérations d'entraînement
+learning_rate = 0.01 # taux d'apprentissage
+epochs = 40 # nombre d'itérations d'entraînement
 Network.train(X, y, learning_rate, epochs)
 
